3c646ffac29fe971236d5979faa7d46856b7dd10
SimaPeter96 <simapeter85@gmail.com>
2023-11-09 00:14:57 +0200

    modified the buttons on the navbar

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from 'react';
 import Img from '../Images/landingIMG.jpg';
 
 function Hero() {
   return (
-    <div className='w-full h-[98vh]'>
-      <img src={Img}  alt="Image" className='w-full h-screen object-cover'  />
+    <div className="w-full h-[98vh] relative">
+      <img src={Img} alt="Image" className="w-full h-full object-cover" />
 
+      <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-5">
+        <div className="text-transparent-opacity-5 p-4 rounded">
+          <h1 className="text-4xl font-bold">MOTHER CITY TOURS</h1>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import Logo from '../Images/TravelAppLogo.png';
 
-
 function Navbar() {
   return (
-    <div className='w-full min-h-[50px] flex justify-between items-center absolute z-10 bg-transparent '>
+    <div className='w-full min-h-[50px] flex justify-between items-center absolute z-10 bg-white bg-opacity-30 shadow-md'>
       <div className="mx-8">
-          <img src={Logo} className="logo" alt='logo'  style={{ width: '80px' }}/>
+        <img src={Logo} className="logo" alt='logo' style={{ width: '80px' }} />
       </div>
-      <div className='flex space-x-4 '>
-          <button className='text-black bg-transparent border-2  border-slate-700 rounded-md'>LOGIN</button>
-          <button className='text-white border-slate-100 rounded-md'>SIGN UP</button>
+      <div className='flex space-x-4 mr-4'>
+        <button className='text-white bg-transparent border-2 border-slate-200 rounded-full'>LOGIN</button>
+        <button className='text-white border-slate-100 rounded-full'>SIGN UP</button>
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
