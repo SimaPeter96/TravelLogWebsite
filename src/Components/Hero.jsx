8bab8f0623e7d56b7381a519cbd48ea60c7b620f
Sima Peter <122967906+SimaPeter96@users.noreply.github.com>
2023-11-08 15:23:30 +0200

    background img added and buttons added

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,1 +1,13 @@
+import React from 'react'
+import Img from '../Images/landingIMG.jpg';
 
+function Hero() {
+  return (
+    <div className='w-full h-[98vh]'>
+      <img src={Img}  alt="Image" className='w-full h-screen object-cover'  />
+
+    </div>
+  )
+}
+
+export default Hero
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,10 +4,15 @@ import Logo from '../Images/TravelAppLogo.png';
 
 function Navbar() {
   return (
-    <div>
-      <div>
-          <img src={Logo} className="logo" alt='logo'/>
+    <div className='w-full min-h-[50px] flex justify-between items-center absolute z-10 bg-transparent '>
+      <div className="mx-8">
+          <img src={Logo} className="logo" alt='logo'  style={{ width: '80px' }}/>
       </div>
+      <div className='flex space-x-4 '>
+          <button className='text-black bg-transparent border-2  border-slate-700 rounded-md'>LOGIN</button>
+          <button className='text-white border-slate-100 rounded-md'>SIGN UP</button>
+      </div>
+      
     </div>
   )
 }
