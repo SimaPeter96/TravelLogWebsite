2dec20ed0fa7ba65bbada757b7bef1deca84acab
Sima Peter <122967906+SimaPeter96@users.noreply.github.com>
2023-11-14 15:18:23 +0200

    added background img to the login page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import BgImg1 from '../Images/BgImage.png'
 
-function Login() {
+const Login = () => {
   return (
-    <div>Login</div>
+    <div className="flex w-full h screen">
+       <div className="w-full  flex items-center justify-centerlg:w-1/2">
+        <img src={BgImg1} alt="" className='w-1/2 h-[100vh]'/>
+           {/* <Form>
+
+           </Form> */}
+           <h1 className='text-black'>Mother</h1>
+         </div>
+         <div className="hidden lg:flex h-full w-1/3">
+
+         </div>
+
+     </div>
+   
   )
 }
 
