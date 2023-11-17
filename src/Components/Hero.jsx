38632de65383da13a0d854b7a258c7dad85c491c
Sima Peter <122967906+SimaPeter96@users.noreply.github.com>
2023-11-17 13:39:27 +0200

    modified hero page

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,8 +7,10 @@ function Hero() {
       <img src={Img} alt="Image" className="w-full h-full object-cover" />
       <div className="absolute inset-0 flex flex-col items-center justify-center">
         <div className="text-center">
-          <h1 className="text-8xl font-medium mx-auto max-w-10xl pb-16 text-black mix-blend-overlay font-poppins">MOTHER CITY TOURS</h1>
-          <p className="text-xl text-white font-extrabold raleway italic ">Experience  Enjoy  Eplore  The  City.</p>
+          <h1 className="text-8xl font-medium mx-auto max-w-10xl pb-4 text-black mix-blend-overlay font-poppins">MOTHER CITY TOURS</h1>
+          <p className="text-xl text-white font-bold italic mb-6" style={{ fontFamily: 'Raleway, sans-serif', wordSpacing: '6px' }}>
+            Experience&nbsp; Enjoy&nbsp; Explore&nbsp;  
+          </p>
         </div>
         <div className="flex space-x-4 mt-10">
           <button className="px-4 py-2 bg-amber-600 text-white shadow-lg rounded-xl text-sm">EXPLORE</button>
