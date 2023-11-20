2a05dbc12efef484334ba314a6418bc4bc21bb66
Sima Peter <122967906+SimaPeter96@users.noreply.github.com>
2023-11-20 10:04:52 +0200

    Added titles on the page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
-import BgImg1 from '../Images/BgImage.png'
-import Bokaap from '../Images/BokaapImg.jpg'
+import React from 'react';
+import BgImg1 from '../Images/BgImage.png';
+import Bokaap from '../Images/BokaapImg.jpg';
 
 const Login = () => {
   return (
-    <div className="w-screen h-screen grid grid-rows 2 md:grid-cols-2">
-       <div className="w-full h-full centered md:h-screen">
-        <img src={BgImg1} alt="" className='flex h-full w-full items-center'/>
-         </div>
-         
-         <div className="w-full h-full centered md:h-screen">
-            <img src={Bokaap} alt="" className=" flex h-full w-full items-center"/>
-         </div>
-     </div>
-   
-  )
-}
+    <div className="w-screen h-screen grid md:grid-cols-2">
+      <div className="relative">
+        <img src={BgImg1} alt="" className="w-full h-full object-cover" />
+        <div className="absolute top-0 left-0 right-0 text-center mt-10">
+          <span className="text-4xl font-bold text-blue mix-blend-overlay font-poppins">
+            MOTHER CITY TOURS
+          </span>
+        </div>
+      </div>
 
-export default Login
+      <div className="relative">
+        <img src={Bokaap} alt="" className="w-full h-full object-cover" />
+        <div className="absolute top-0 left-0 right-0 text-center mt-10">
+          <span className="text-6xl font-bold text-blue mix-blend-overlay font-poppins">
+            WELCOME BACK 
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
