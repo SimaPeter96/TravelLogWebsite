171309b1dcd32ce5f70cffbac9dc3b8184e6f390
Sima Peter <122967906+SimaPeter96@users.noreply.github.com>
2023-11-22 15:27:45 +0200

    modified sign up page

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import ImgBg from '../Images/'
+import ImgBg from '../Images/wineImg.jpg'
 
 function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='w-screen h-screen flex'>
+      <div className='w-1/2 relative'>
+      <img src={ImgBg} alt="WineImg" className="w-full h-full object-cover" />
+      </div>
+
+    
+    </div>
   )
 }
 
