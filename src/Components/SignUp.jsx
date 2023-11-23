67fb71b55b6e3e3e21e40a3c3d5bcf367e7e7e4e
Sima Peter <122967906+SimaPeter96@users.noreply.github.com>
2023-11-23 12:12:40 +0200

    modified signup page

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -7,6 +7,16 @@ function SignUp() {
       <div className='w-1/2 relative'>
       <img src={ImgBg} alt="WineImg" className="w-full h-full object-cover" />
       </div>
+      <div className='absolute top-2 left-0 right-0 text-left mt-10 ml-10'>
+        <div className='SignUpHeading py-12'>
+          <span className='text-4xl font-bold mt-32 ml-32 text-blue mix-blend-overlay font-poppins'>
+            MOTHER CITY TOURS
+          </span>
+          <div className='slogan ps-48'>
+          <h2 className='text-md font-bold  text-blue mix-blend-overlay font-poppins'>EXPERIENCE ENJOY EXPLORE</h2>
+          </div>
+        </div>
+      </div>
 
     
     </div>
