ec3dbf371b2d70819df00864e1e236ce6907052b
Sima Peter <122967906+SimaPeter96@users.noreply.github.com>
2023-12-01 13:21:52 +0200

    modified

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -58,53 +58,74 @@ function SignUp() {
           <h2 className='text-md font-bold  text-white font-poppins'>Create your account</h2>
         </div>
         <div className='ml-16'>
-        <form action='#'>
+        <form action='#' onSubmit={handleSubmit}>
           <div className='grid grid-cols-2 gap-5'>
             <input type="text" 
             placeholder="Firstname" 
-            className='border border-gray-400 p-1 px-2 rounded'>
-            </input>
+            className='border border-gray-400 p-1 px-2 rounded'
+            value={firstName}
+            onChange={(e)=> setFirstName(e.target)}
+            ></input>
             <input type="text" 
             placeholder="Surname" 
-            className='border border-gray-400 p-1 px-2 rounded'>
-            </input>
+            className='border border-gray-400 p-1 px-2 rounded'
+            value={surname}
+            onChange={(e) => setSurname(e.target.value)}
+            ></input>
           </div>
           <div className='mt-5'>
-            <input type="text" placeholder="Phone number" className='border border-gray-400 py-1 px-2 w-full rounded'></input>
+            <input type="text" 
+            placeholder="Phone number" 
+            className='border border-gray-400 py-1 px-2 w-full rounded'
+            value={phoneNuumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            ></input>
           </div>
           <div className='mt-5'>
-            <input type="text" placeholder="Email" className='border border-gray-400 py-1 px-2 w-full rounded'></input>
+            <input type="text" 
+            placeholder="Email" 
+            className='border border-gray-400 py-1 px-2 w-full rounded'
+            value={email}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            ></input>
           </div>
           <div className='mt-5'>
-            <input type="text" placeholder="Create password" className='border border-gray-400 py-1 px-2 w-full rounded'></input>
+            <input type="text" 
+            placeholder="Create password" 
+            className='border border-gray-400 py-1 px-2 w-full rounded'
+            ></input>
           </div>
           <div className='mt-5'>
-            <input type="text" placeholder="Confirm password" className='border border-gray-400 py-1 px-2 w-full rounded'></input> 
+            <input type="text" 
+            placeholder="Confirm password" 
+            className='border border-gray-400 py-1 px-2 w-full rounded'
+            value={password}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            ></input> 
           </div>
           <div className='mr-24'>
               <input type='checkbox' name='ch'  id='ch' className='mr-2'/>
               <span className="text-md text-white">I accept the Terms of Use & Privacy Policy </span>
           </div>
           <div className='mt-5'>
-            <button className='w-full bg-transparent border border-white py-2 text-white hover:bg-blue-400 rounded shadow-inner  '>SIGN UP</button>
+            <button 
+            type='submit'
+            className='w-full bg-transparent border border-white py-2 text-white hover:bg-blue-400 rounded shadow-inner  ' 
+            >
+              SIGN UP
+              </button>
           </div>
           <div className='mt-5'>
             <button className='w-full bg-white border border-white py-2 text-black hover:bg-transparent rounded shadow-inner '>
             <img src={google} alt='' className='w-6 h-6 inline mr-2' />
-              SIGN UP WITH GOOGLE</button>
+              SIGN UP WITH GOOGLE
+              </button>
           </div>
-            
-         
-        </form>
+         </form>
       </div>
-        
+       </div>
       </div>
-     
-     
-        
-      </div>
-      </div>
-  
+    </div>
   )
 }
 
